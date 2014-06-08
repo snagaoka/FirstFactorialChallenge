084b009974c1e410f1a753f07db499b0f571464c
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-07 14:22:15 -1000

    for loop factorial working

--- js/FirstFactorialChallenge.js
+++ js/FirstFactorialChallenge.js
@@ -4,57 +4,15 @@
 	var button = document.getElementById("calculate");
 
 	button.onclick = function () {
+		var result = 1;
 		var factorialnumber = document.getElementById("num").value;
-		for (var i = 0; i < factorialnumber; i++) {
-			factorialnumber * i
-			// factorialnumber *= i;
-			// console.log(factorialnumber * i)
-			var result = factorialnumber * i
-			result * i
-			console.log(result * i)
+		for (var i = 1; i <= factorialnumber; i++) {
+			result = i * result;
 		}
+		console.log(result);
+		answer.innerHTML = result;
 	}
 
-
 })(); 
-
-/*	
-
-button.onclick = function factorial(num) {
-			if (num < 0) {
-				return -1;
-			}
-			else if (num == 0) {
-				return 1;
-			}
-			else {
-				return (num * factorial(num-1));
-			}
-		}
-
-		var result = factorial(num);
-		document.write(result);  
-*/
-
-
-	//alert("Factorial of " + n + " is " + product)
-
-/*	function factnum (n) {
-		if ((n == 0) || (n == 1))
-			return 1
-		else {
-			result = (n * factorial(n-1) )
-			return result
-		}
-	}*/
-	
-	// }
-
-	// 	var n = 5
-	// var product = 1
-
-	// for (i = 2; i <= n; i++){
-		// product = product * i
-	// }
 
 
