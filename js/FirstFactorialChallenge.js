e77a536d61044f983fc67b446e9943e7556c3240
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-07 14:54:09 -1000

    added error message to non-number inputs

--- js/FirstFactorialChallenge.js
+++ js/FirstFactorialChallenge.js
@@ -6,11 +6,20 @@
 	button.onclick = function () {
 		var result = 1;
 		var factorialnumber = document.getElementById("num").value;
-		for (var i = 1; i <= factorialnumber; i++) {
-			result = i * result;
+		var notValid = isNaN(factorialnumber)
+
+		if (notValid) {
+			answer.innerHTML = "error";
 		}
-		console.log(result);
-		answer.innerHTML = result;
+
+		else {
+			for (var i = 1; i <= factorialnumber; i++) {
+			result = i * result;
+			}
+			console.log(result);
+			answer.innerHTML = result;
+		}
+
 	}
 
 })(); 
